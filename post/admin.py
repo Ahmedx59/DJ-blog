1f0e10b3abc3f5a16e6b640297bf57686d5566f9
Ahmedx59 <Ahmedx59@gmail.com>
2024-07-22 16:15:24 +0300

    add more models

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+
+class Postadmin(admin.ModelAdmin):
+    list_display = ('title','discription','created_at')
+    search_fields = ('title',)
+    list_filter = ('active',)
+
+
+admin.site.register(Post,Postadmin)
--- post/models.py
+++ post/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    discription = models.TextField(max_length=1000)
+    created_at = models.DateField()
+    active = models.BooleanField(default=False)
 
+
+    def __str__(self):
+        return self.title
