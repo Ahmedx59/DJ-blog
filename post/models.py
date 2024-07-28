01832f8d1995bdcc4954d6763174e54aa4cc5574
Ahmedx59 <Ahmedx59@gmail.com>
2024-07-28 05:21:30 +0300

    add more models

--- post/models.py
+++ post/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Post(models.Model):
+    auth = models.ForeignKey(User , on_delete=models.CASCADE , related_name='post_user')
     title = models.CharField(max_length=200)
     discription = models.TextField(max_length=1000)
     created_at = models.DateField()
     active = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='posts/')
 
 
     def __str__(self):
